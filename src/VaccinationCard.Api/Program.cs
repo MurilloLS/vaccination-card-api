9ff1ff076fe4b69032ee155916247e462deaf2cb
Murillo <murillo.lds01@gmail.com>
2025-11-22 10:39:57 -0300

    🔧 chore(api): hide root redirect endpoint from Swagger UI documentation

--- src/VaccinationCard.Api/Program.cs
+++ src/VaccinationCard.Api/Program.cs
@@ -94,7 +94,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    app.MapGet("/", () => Results.Redirect("/swagger"));
+    app.MapGet("/", () => Results.Redirect("/swagger")).ExcludeFromDescription();
 }
 
 app.UseExceptionHandler();
